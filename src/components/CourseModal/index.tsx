718a4179e9bf87d404bc9c32e10512d6c84014b5
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2022-04-26 08:44:17 -0300

    add CourseModal

---
+++ src/components/CourseModal/index.tsx
@@ -0,0 +1,106 @@
+import { Dispatch, SetStateAction } from 'react';
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import api from '../../services/api';
+import styles from './styles.module.scss'
+
+interface ModalProps {
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
+  courseData: CourseData;
+  setUpdatePage: Dispatch<SetStateAction<boolean>>
+}
+
+interface CourseData {
+  id: string,
+  title: string,
+  durationInMinutes: number,
+  duration: string,
+  teacher: {
+    id: string,
+    name: string
+  }
+  questions: {
+    id: string,
+    text: string,
+    student: {
+      id: string,
+      name: string,
+    },
+    ansers: {
+      id: string,
+      text: string,
+    }[]
+  }[]
+}
+
+export function CourseModal({ setModalOpen, courseData, setUpdatePage }: ModalProps) {
+  const { register, handleSubmit } = useForm()
+
+  const handleQuestionSubmit = async ({ text }) => {
+    if (!text) toast.error('Digite algo na sua pergunta.')
+    try {
+      await api.post('/course/question', {
+        courseId: courseData.id,
+        text
+      })
+
+      toast.success('Pergunta cadastrada com sucesso')
+      setUpdatePage(current => !current)
+      setModalOpen(false)
+    } catch (err) {
+      toast.error(err.response.data.error)
+    }
+  }
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1>Perguntas</h1>
+          <p className={styles.subTitle}>Envie até 2 perguntas por curso.</p>
+          <hr />
+          <div className={styles.courseInfo}>
+            <p>Curso: <span>{courseData.title}</span> - Duração <span>{courseData.duration}Hrs</span></p>
+            <p>Professor: <span>{courseData.teacher.name}</span></p>
+          </div>
+          <ul>
+            <h2>Perguntas</h2>
+            {courseData.questions.map(question => {
+              return (
+                <li key={question.id}>
+                  <p>{question.student.name} pergunta: </p>
+                  <p>{question.text}</p>
+                  {question.ansers.map(anser =>
+                    <p key={anser.id}>
+                      <span>Resposta: </span> {anser.text}
+                    </p>)}
+                </li>
+              )
+            })}
+          </ul>
+
+          <form className={styles.form} onSubmit={handleSubmit(handleQuestionSubmit)}>
+            <textarea
+              {...register('text')}
+              name="text"
+              id="text"
+              cols={30} rows={5}
+              placeholder="Digite sua pergunta aqui..."
+            >
+            </textarea>
+
+            <button type="submit">Perguntar</button>
+          </form>
+        </div>
+        <button
+          onClick={() => setModalOpen(false)}
+          className={styles.closeButton}
+        >
+          +
+        </button>
+
+      </div>
+    </div>
+  )
+}
--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,21 +1,36 @@
+import { toast } from 'react-toastify';
 import { GetServerSideProps } from "next"
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Pagination } from "../../components/pagination";
 import api from "../../services/api"
 import ConverterMinutesToHours from "../../utils/ConverterMinutesToHours";
 
 import styles from './styles.module.scss'
+import { AuthContext } from '../../contexts/AuthContext';
+import { CourseModal } from '../../components/CourseModal';
 
 interface CoursesProps {
-  id: string;
-  title: string;
+  id: string,
+  title: string,
   durationInMinutes: number,
-  duration: string;
+  duration: string,
   teacher: {
     id: string,
     name: string
   }
+  questions: {
+    id: string,
+    text: string,
+    student: {
+      id: string,
+      name: string,
+    },
+    ansers: {
+      id: string,
+      text: string,
+    }[]
+  }[]
 }
 
 interface PagesProps {
@@ -23,10 +38,12 @@ interface PagesProps {
   take: number
 }
 
-
-
 export default function Courses() {
+  const { isAuthenticated } = useContext(AuthContext)
   const [courses, setCourses] = useState<CoursesProps[]>([])
+  const [courseSelected, setCourseSelected] = useState<CoursesProps | null>(null)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [updatePage, setUpdatePage] = useState(false)
   const [offset, setOffset] = useState<number>(0)
   const [pagesProps, setPagesProps] = useState<PagesProps>({
     take: 5,
@@ -58,12 +75,25 @@ export default function Courses() {
           top: 0,
           behavior: "smooth"
         })
-      } catch (error) {
-        //TODO tratativa de erro
+      } catch (e) {
+        console.log(e.response)
       }
     }
     loadData()
-  }, [offset])
+  }, [offset, updatePage])
+
+
+  const handleOpenCourse = (courseData: CoursesProps) => {
+    if (!isAuthenticated) {
+      toast.error('Realize o login na tela inicial, para ter acesso às perguntas.')
+      return
+    }
+    setCourseSelected(courseData)
+    setModalOpen(true)
+
+
+  }
+
 
   return (
     <>
@@ -73,7 +103,7 @@ export default function Courses() {
       <main className={styles.main}>
         <ul className={styles.ul}>
           {courses.map(course =>
-            <li key={course.id}>
+            <li key={course.id} onClick={() => handleOpenCourse(course)}>
               <h1>Curso: {course.title} <span>- {course.duration}</span></h1>
               <p>Profesor: <span>{course.teacher.name}</span></p>
             </li>)}
@@ -85,6 +115,13 @@ export default function Courses() {
           setOffset={setOffset}
         />
       </main>
+      {(modalOpen && !!courseSelected) && (
+        <CourseModal
+          setModalOpen={setModalOpen}
+          courseData={courseSelected}
+          setUpdatePage={setUpdatePage}
+        />
+      )}
     </>
   )
 }
