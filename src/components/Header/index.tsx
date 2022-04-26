47cc5347a23567cff0ced517c888258ddef11f79
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2022-04-25 21:58:55 -0300

    update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,28 @@
+import Link from 'next/link'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 import styles from './styles.module.scss'
 export function Header() {
+  const { user, singOut } = useContext(AuthContext)
+
   return (
     <header className={styles.headerContainer}>
       <h1>LXP</h1>
       <nav>
-        <a href="#">Início</a>
-        <a href="#">Cursos</a>
-        <a className={styles.login} href="/">Entrar</a>
-        <a className={styles.exit} href="/courses">Sair</a>
+        <Link href="/">Início</Link>
+        <Link href="/courses">Cursos</Link>
+        {(user?.userType === 'teacher'
+          || user?.userType === "adm"
+        ) && (
+            <Link href="/dashboard">
+              <a className={styles.dashboard} >Dashboard</a>
+            </Link>
+          )}
+        {!!user.name && (
+          <button className={styles.exitButton} onClick={singOut}>
+            Sair
+          </button>
+        )}
       </nav>
     </header>
   )
