7bcc40086d475322e6318b2679ee51a96a84ee10
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2022-04-26 08:44:30 -0300

    remove api

--- src/pages/api/courses.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse} from 'next'
-import api from '../../services/api'
-
-export default async (request: NextApiRequest, response: NextApiResponse) => {
-  const courses = await api.get('course');
-
-  return response.json(courses.data)
-}
